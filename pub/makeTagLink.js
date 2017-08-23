ad7a5e783efa2adec985d8b7669b20ab15b981c5
sam-meng <f95942117@gmail.com>
2017-08-23 13:33:57 +0200

    [Tag] JS file done

---
+++ pub/makeTagLink.js
@@ -0,0 +1,18 @@
+
+// Find all the hash tag elements and modify their text & href
+document.addEventListener('DOMContentLoaded', function()
+{
+  var linkElementList = document.querySelectorAll("a");
+  var linkElementListLen = linkElementList.length;
+  for (var i = 0; i < linkElementListLen; i++)
+  {
+    // A hash tag is identified by the following condition
+    var id = linkElementList[i].id;
+    if (id !== "" && id === linkElementList[i].name && linkElementList[i].href === "")
+    {
+      linkElementList[i].textContent = "#" + id;
+      linkElementList[i].href = location.origin + "/Site/SearchE?q=%23" + id +
+      "&unit=para&markup=on&header=full&title=Search+Results%3A&matchnum=1&timer=1&action=search&fmt=extract";
+    }
+  }
+});
